a4f3b561e6a95c1a820bba28b35f484935dd928e
HamzaLatif02 <hamza.latif@city.ac.uk>
2023-04-03 04:02:33 +0100

    added discount plan page functionality

--- src/AirViaLtd/DiscountPlanPage.java
+++ src/AirViaLtd/DiscountPlanPage.java
@@ -1,6 +1,9 @@
 package AirViaLtd;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class DiscountPlanPage {
     private JPanel mainPanel;
@@ -9,22 +12,163 @@ public class DiscountPlanPage {
     private JLabel selectCustomerLabel;
     private JComboBox customerComboBox;
     private JLabel selectMonthLabel;
-    private JComboBox monthComboBox;
     private JTextField salesTextField;
     private JLabel salesDuringThisPeriodLabel;
     private JComboBox discountPlanComboBox;
     private JLabel selectDiscountPlanLabel;
-    private JTextField discountAmountTextField;
-    private JLabel discountAmountLabel;
+    private JTextField discountPercentageTextField;
+    private JLabel discountPercentageLabel;
     private JButton setDiscountButton;
+    private JComboBox monthComboBox;
+    private JComboBox yearComboBox;
+    private JLabel selectYearLabel;
+    private JButton getSalesButton;
+
 
     private AirViaLtd app;
 
     public DiscountPlanPage(AirViaLtd a) {
         this.app = a;
+        addCustomerComboBoxData();
+        addMonthComboBox();
+        addYearComboBox();
+        addGetSalesListener();
+        addDiscountPlanComboBoxData();
+        addSetDiscountButtonListener();
     }
 
     public JPanel getMainPanel() {
         return mainPanel;
+    }
+
+
+    public void addCustomerComboBoxData(){
+        customerComboBox.addItem("-- Select --");
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306",
+                    "in2018g16_d",
+                    "35cnYJLB");
+
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery("select EmailAddress, FirstName, LastName FROM in2018g16.Customer");
+
+            while (rs.next()){
+                customerComboBox.addItem(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+            }
+            con.close();
+        } catch (Exception e) { System.out.println(e);}
+    }
+
+    public void addMonthComboBox(){
+        monthComboBox.addItem("-- Select --");
+
+        monthComboBox.addItem("January");
+        monthComboBox.addItem("February");
+        monthComboBox.addItem("March");
+        monthComboBox.addItem("April");
+        monthComboBox.addItem("May");
+        monthComboBox.addItem("June");
+        monthComboBox.addItem("July");
+        monthComboBox.addItem("August");
+        monthComboBox.addItem("September");
+        monthComboBox.addItem("October");
+        monthComboBox.addItem("November");
+        monthComboBox.addItem("December");
+
+
+    }
+
+    public void addYearComboBox(){
+        yearComboBox.addItem("-- Select --");
+
+        for (int i = 2023; i > 1999; i--){
+            yearComboBox.addItem(i);
+        }
+    }
+
+    public void addGetSalesListener(){
+        getSalesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addSalesDuringPeriodData();
+            }
+        });
+
+    }
+    public void addSalesDuringPeriodData(){
+
+        String cu = customerComboBox.getSelectedItem().toString();
+        String[] splitCuSelected = cu.split("\\s+");
+        String email = (splitCuSelected[0]);
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306",
+                    "in2018g16_d",
+                    "35cnYJLB");
+
+            String sql = "select count(TicketNumber) from in2018g16.Sale where CustomerEmailAddress = ? and SaleDate > ?-? and SaleDate < ?-?;";
+            PreparedStatement stmt=con.prepareStatement(sql);
+
+            stmt.setString(1, email);
+            stmt.setInt(2, Integer.valueOf(yearComboBox.getSelectedItem().toString()));
+            stmt.setInt(3, monthComboBox.getSelectedIndex());
+            stmt.setInt(4, Integer.valueOf(yearComboBox.getSelectedItem().toString()));
+            stmt.setInt(5, monthComboBox.getSelectedIndex() + 1);
+
+            ResultSet rs=stmt.executeQuery();
+
+            while (rs.next()){
+                salesTextField.setText(rs.getString(1));
+            }
+            con.close();
+        } catch (Exception e) { System.out.println(e);}
+    }
+
+    public void addDiscountPlanComboBoxData(){
+        discountPlanComboBox.addItem("-- Select --");
+        discountPlanComboBox.addItem("Fixed");
+        discountPlanComboBox.addItem("Flexible");
+    }
+
+    public void addSetDiscountButtonListener(){
+        setDiscountButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String cu = customerComboBox.getSelectedItem().toString();
+                String[] splitCuSelected = cu.split("\\s+");
+                String email = (splitCuSelected[0]);
+
+                try{
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection con= DriverManager.getConnection(
+                            "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306",
+                            "in2018g16_d",
+                            "35cnYJLB");
+
+                    String sql = "update in2018g16.Customer Set ? = ? Where EmailAddress = ?";
+                    PreparedStatement stmt=con.prepareStatement(sql);
+
+                    stmt.setString(1, discountPlanComboBox.getSelectedItem().toString() + "Discount");
+                    stmt.setInt(2, Integer.valueOf(discountPercentageTextField.getText()));
+                    stmt.setString(3, email);
+
+
+                    int rs=stmt.executeUpdate();
+
+                    if (rs != 0){
+                        JOptionPane.showMessageDialog(null, "Discount added successfully", "Success", JOptionPane.PLAIN_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Could not add discount at the moment, please retry", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                    con.close();
+                } catch (Exception x) { System.out.println(x);}
+            }
+        });
     }
 }
