1a53074547a1f392adfbe9711f8ff4504eadc052
HamzaLatif02 <hamza.latif@city.ac.uk>
2023-04-03 15:43:01 +0100

    added data in create report combobox fields

--- src/AirViaLtd/CreateReportPage.java
+++ src/AirViaLtd/CreateReportPage.java
@@ -1,6 +1,10 @@
 package AirViaLtd;
 
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class CreateReportPage {
     private JPanel mainPanel;
@@ -24,9 +28,48 @@ public class CreateReportPage {
 
     public CreateReportPage(AirViaLtd a) {
         this.app = a;
+        addSelectReportData();
+        addIndividualOrGlobal();
+        addTravelAdvisorData();
     }
 
     public JPanel getMainPanel() {
         return mainPanel;
+    }
+
+    public void addSelectReportData(){
+        selectReportComboBox.addItem("-- Select --");
+        selectReportComboBox.addItem("Ticket Stock Turnover");
+        selectReportComboBox.addItem("Interline Sales");
+        selectReportComboBox.addItem("Global Sales");
+    }
+
+    public void addIndividualOrGlobal(){
+
+        individualGlobalComboBox.addItem("-- Select --");
+        individualGlobalComboBox.addItem("Individual");
+        individualGlobalComboBox.addItem("Global");
+
+    }
+
+    public void addTravelAdvisorData(){
+
+        travelAdvisorComboBox.addItem("-- Select -- ");
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306",
+                    "in2018g16_d",
+                    "35cnYJLB");
+
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery("select AdvisorCode, FirstName, LastName FROM in2018g16.TravelAdvisor WHERE AdvisorCode != 1");
+
+            while (rs.next()){
+                travelAdvisorComboBox.addItem(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+            }
+            con.close();
+        } catch (Exception e) { System.out.println(e);}
     }
 }
